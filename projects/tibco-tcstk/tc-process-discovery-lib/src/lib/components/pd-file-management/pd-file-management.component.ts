0db42490fcdcb5c66aa058fa556b7f49e6f7b4e8
Manuel Carlos Rodríguez Álvarez-Querol <mcrodrig@manuels-mbp.emea.tibco.com>
2019-07-29 08:46:17 +0200

    Add collaboration widgets.

--- projects/tibco-tcstk/tc-process-discovery-lib/src/lib/components/pd-file-management/pd-file-management.component.ts
+++ projects/tibco-tcstk/tc-process-discovery-lib/src/lib/components/pd-file-management/pd-file-management.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, EventEmitter } from '@angular/core';
 import { MessageQueueService } from '@tibco-tcstk/tc-core-lib';
 import { TcDocumentService, Document, DocumentAction } from '@tibco-tcstk/tc-liveapps-lib';
 import { MatDialog } from '@angular/material';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -17,17 +17,28 @@ export class PdFileManagementComponent implements OnInit {
     public documents: Document[];
     public  customActions: string[]
 
+    public sandboxId: number;
+
+    public uiAppId: string; 
+    public userId: string;
+
     constructor(
         private messageService: MessageQueueService,
         private documentService: TcDocumentService,
         public dialog: MatDialog,
-        private router: Router
+        private router: Router,
+        private route: ActivatedRoute
     ) { }
 
     ngOnInit() {
         this.messageService.sendMessage('title-bar', 'file-management');
+        this.sandboxId = this.route.snapshot.data.claims.sandboxId;
         // this.listFiles();
         this.customActions = ['Create business process'];
+
+        this.uiAppId = this.route.snapshot.data.generalConfigHolder.uiAppId;
+        this.userId = this.route.snapshot.data.claims.userId;
+
     }
 
     createNewDatasource() {
@@ -39,7 +50,7 @@ export class PdFileManagementComponent implements OnInit {
             {
                 documentName: $event.document.name,
                 documentExtension: $event.document.extension,
-                documentURL: window.location.protocol + '//' + window.location.host + this.documentService.getUrlForDocument('orgFolders', this.FOLDERID, $event.document.name, null, 31)
+                documentURL: window.location.protocol + '//' + window.location.host + this.documentService.getUrlForDocument('orgFolders', this.FOLDERID, $event.document.name, null, this.sandboxId)
             }
         ], {});        
     }
