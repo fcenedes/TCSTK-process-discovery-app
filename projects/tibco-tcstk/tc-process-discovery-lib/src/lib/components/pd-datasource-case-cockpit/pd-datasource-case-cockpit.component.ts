e26ef9478eeabebf8e8e0236ee6b9e05356ccf75
Manuel Carlos Rodríguez Álvarez-Querol <mcrodrig@manuels-mbp.emea.tibco.com>
2020-02-28 15:45:37 +0100

    Move from OnInit to OnChanges.

--- projects/tibco-tcstk/tc-process-discovery-lib/src/lib/components/pd-datasource-case-cockpit/pd-datasource-case-cockpit.component.ts
+++ projects/tibco-tcstk/tc-process-discovery-lib/src/lib/components/pd-datasource-case-cockpit/pd-datasource-case-cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { LiveAppsCaseCockpitComponent } from '@tibco-tcstk/tc-liveapps-lib';
 
 @Component({
@@ -6,23 +6,22 @@ import { LiveAppsCaseCockpitComponent } from '@tibco-tcstk/tc-liveapps-lib';
   templateUrl: './pd-datasource-case-cockpit.component.html',
   styleUrls: ['./pd-datasource-case-cockpit.component.css']
 })
-export class PdDatasourceCaseCockpitComponent extends LiveAppsCaseCockpitComponent implements OnInit {
 
-    ngOnInit() {
+export class PdDatasourceCaseCockpitComponent extends LiveAppsCaseCockpitComponent {
+    ngOnChanges(changes: SimpleChanges): void {
         // disable fav button
         this.incFavButton = false;
         this.incConfigButton = false;
         this.incRefreshButton = false;
         // add a new button
-        
+
         // setup button array
         this.toolbarButtons = [];
 
         // Remove tabs
         this.showDocuments = true;
-        
-        // call superclass init
-        super.ngOnInit();
-    }
 
+        // call superclass init
+        super.ngOnChanges(changes);
+    }
 }
